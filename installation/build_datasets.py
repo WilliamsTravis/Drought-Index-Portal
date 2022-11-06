542734fd27d04167f65fae50cbd3e6c35523c92b
williamstravis <twillico@proton.me>
2022-11-06 14:27:06 -0700

    Updated environment and build_dataset.py

--- installation/build_datasets.py
+++ installation/build_datasets.py
@@ -2,13 +2,13 @@
 """Download rainfall and drought indices."""
 from drip.app.options.options import INDEX_NAMES
 from drip.downloaders.utilities import Data_Builder
-from drip.loggers import init_logger, set_handler
+from drip.loggers import init_logger
 
 logger = init_logger(__name__)
 
 
-def get_wwdt():
-    """Download all WWDT datasets specified in drip.options."""
+def main():
+    """Download and format all needed files for the prf app."""
     for index, desc in INDEX_NAMES.items():
         if not index.startswith("ri"):
             if "Index" in desc:
@@ -16,21 +16,6 @@ def get_wwdt():
                 print(f"Building {index}...")
                 builder = Data_Builder(index)
                 builder.build(overwrite=False)
-
-
-
-def main():
-    """Download and format all needed files for the prf app."""
-    # Download each index we have labels for
-    get_wwdt()
-
-    # Download EDDI
-
-    # Download Gridmet
-
-    # Download LERI
-
-    # Download USDM (custom dataset)
 
 
 if __name__ == "__main__":
